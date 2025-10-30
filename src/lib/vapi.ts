fcb501ec130aebb6fdbf6c40f0f29f4f55a8169a
Pyush yadav <pyushyadav2004@gmail.com>
2025-10-31 01:11:17 +0530

    AI fitness

---
+++ src/lib/vapi.ts
@@ -0,0 +1,3 @@
+import Vapi from "@vapi-ai/web";
+// now we are using vapi varible to check if ai is talking or responding and many more
+export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
